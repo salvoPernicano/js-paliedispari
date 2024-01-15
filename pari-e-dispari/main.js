f91923bc619995c5cfa41d8f60999dcfe8e60aa5
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-15 16:23:46 +0100

    fix struttura / feat  funzioni

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,3 +1,20 @@
-let evenChoice = document.getElementById("even");
-let oddChoice = document.getElementById("odd");
+let evenOddTotal = document.getElementById("evenOdd");
+let userNumber = document.getElementById("number")
 
+
+function pcNumber(min,max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function play(){
+    let computerNumber = pcNumber(1,5);
+    let result = parseInt(userNumber.value) + computerNumber;
+    if (evenOddTotal.value === "even" && result % 2 === 0){
+        alert("you won!")
+    }
+    else if (evenOddTotal.value === "odd" && result % 2 !== 0){
+        alert("you won!")
+    } else 
+    alert("you lost!")
+    console.log(result);
+}
